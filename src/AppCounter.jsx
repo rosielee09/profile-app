74c6f37f5585574d46ba842cd4df4a85f98ae45f
rosie <rosie.bh.lee@gmail.com>
2023-07-10 16:15:53 -0700

    add appcounter component including total count

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -5,6 +5,7 @@ import Counter from './components/Counter';
 export default function AppCounter() {
   const [count, setCount] = useState(0);
   const handleClick = () => setCount((prev) => prev + 1);
+
   return (
     <div className="container">
       <div className="banner">
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
-export default function Counter({ total, onClick }) {
+export default function Counter({total, onClick}) {
   const [count, setCount] = useState(0);
   return (
     <div className="counter">
-      <p className="number">
-        {count} <span className="total">/{total}</span>
+      <p className="number">{count}
+         <span className="total">/{total}</span>
       </p>
       <button
         className="button"
