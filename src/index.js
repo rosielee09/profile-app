033064fa19e1505812d79510f07b40fff84f2229
rosie <rosie.bh.lee@gmail.com>
2023-07-11 15:34:20 -0700

    add products components using useEffect

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppProfile from './AppProfile';
 import AppCounter from './AppCounter';
+import AppProducts from './AppProducts';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppCounter />
+    <AppProducts />
   </React.StrictMode>
 );
 
